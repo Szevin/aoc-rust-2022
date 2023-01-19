6b0d15512aaa5aaa6241cc65224178b91e25a1f0
Geiger Kevin <kevingeiger@smartx.eu>
2023-01-19 22:39:20 +0100

    day 3 part_b done!

--- src/day3.rs
+++ src/day3.rs
@@ -23,5 +23,31 @@ pub fn solve_a() -> u32 {
 }
 
 pub fn solve_b() -> u32 {
-    todo!()
+    std::fs::read_to_string("input/day3.txt")
+        .expect("File not found!")
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|c| {
+            let (first, second, third) = (
+                c[0].chars().collect::<Vec<char>>(),
+                c[1].chars().collect::<Vec<char>>(),
+                c[2].chars().collect::<Vec<char>>(),
+            );
+
+            first
+                .iter()
+                .find(|x| second.contains(x) && third.contains(x))
+                .unwrap()
+                .clone()
+        })
+        .map(|c| {
+            if c.is_lowercase() {
+                (c as u8) - (b'a' - 1)
+            } else {
+                (c as u8) + 27 - (b'A')
+            }
+        })
+        .map(|x| x as u32)
+        .sum()
 }
