a59d97609db3a324ff38cd5e49e3cfcb8a37ac54
Geiger Kevin <kevingeiger@smartx.eu>
2023-01-21 17:23:59 +0100

    day 4 part_a done!

---
+++ src/day4.rs
@@ -0,0 +1,28 @@
+pub fn solve_a() -> u32 {
+    std::fs::read_to_string("input/day4.txt")
+        .expect("File not found!")
+        .lines()
+        .map(|s| s.split_once(",").unwrap())
+        .map(|(f, s)| (f.split_once("-").unwrap(), s.split_once("-").unwrap()))
+        .map(|((f_s, f_e), (s_s, s_e))| {
+            (
+                (f_s.parse::<u32>().unwrap(), f_e.parse::<u32>().unwrap()),
+                (s_s.parse::<u32>().unwrap(), s_e.parse::<u32>().unwrap()),
+            )
+        })
+        .fold(0, |sum, x| {
+            let ((first_start, first_end), (second_start, second_end)) = x;
+            if first_start >= second_start && first_end <= second_end {
+                sum + 1
+            } else if first_start <= second_start && first_end >= second_end {
+                sum + 1
+            } else {
+                println!("{:?}", x);
+                sum
+            }
+        })
+}
+
+pub fn solve_b() -> u32 {
+    todo!()
+}
